5748126ad12f0899c5f9ab1eb594f1db9d9c273f
琼汐音 <3475238125@qq.com>
2026-07-15 20:34:27 +0800

    添加屏幕抖动效果

---
+++ Assets/Shader/scene_effect.fs
@@ -0,0 +1,49 @@
+#version 430
+in vec2 f_TexCoord;
+out vec4 FragColor;
+uniform vec2 u_offsets[9];
+uniform int u_edge_kernel[9];
+uniform float u_blur_kernel[9];
+uniform bool u_shake;
+uniform bool u_chaos;
+uniform bool u_confuse;
+uniform sampler2D u_scene;
+
+void main()
+{
+    FragColor =vec4(0.0f);
+    vec3 sampler[9];
+    if(u_chaos || u_shake)
+    {
+        for(int i =0;i<9;++i)
+        {
+            sampler[i] = vec3(texture(u_scene,f_TexCoord + u_offsets[i]));
+        }
+    }
+    if(u_chaos)
+    {
+        for(int i=0;i<9;++i)
+        {
+            FragColor+=vec4(sampler[i]*u_edge_kernel[i],0.0f);
+        }
+            FragColor.a = 1.0f;
+
+    }
+    else if(u_confuse)
+    {
+        FragColor = texture(u_scene,vec2(1-f_TexCoord.x,1-f_TexCoord.y));
+    }
+    else if(u_shake)
+    {
+        for(int i =0;i<9;++i)
+        {
+            FragColor+=vec4(sampler[i]*u_blur_kernel[i],0.0f);
+        }
+            FragColor.a = 1.0f;
+
+    }
+    else
+    {
+        FragColor = texture(u_scene,f_TexCoord);
+    }
+}
